552e7bcfcfb182fdd516c5a301d74851666c739c
Avery Olsen <averyo@Averys-MacBook-Pro.local>
2025-03-03 13:58:23 -0700

    Added UMLs and chap10

--- chap9/Exercise09_01.java
+++ chap9/Exercise09_01.java
@@ -1,14 +1,21 @@
-public class exercise0901 {
+public class Exercise09_01 {
     public static void main(String[] args) {
+        Rectangle rectA = new Rectangle(40.0, 4.0);
+        printRectangle("A", rectA);
+        Rectangle rectB = new Rectangle(35.9, 3.5);
+        printRectangle("B", rectB);
         Rectangle[] rectArray = new Rectangle[10];
-        for (i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             double rectheight = Math.random() * 11.0;
             double rectwidth = Math.random() * 11.0;
             rectArray[i] = new Rectangle(rectheight, rectwidth);
-            System.out.println("\tRectangle %2d", (i + 1));
-            System.out.printf("Height: %6.2f | Weight: %6.2f\n", rectArray[i].getHeight(), rectArray[i].getWidth());
-            System.out.printf("Area  : %6.2f | Perim.: %6.2f\n", rectArray[i].getArea(), rectArray[i].getPerimeter());
+            printRectangle(Integer.toString(i + 1), rectArray[i]);
         }
+    }
+    static void printRectangle(String name, Rectangle object) {
+        System.out.printf("          Rectangle %2s\n", name);
+        System.out.printf("Height: %6.2f | Width : %6.2f\n", object.getHeight(), object.getWidth());
+        System.out.printf("Area  : %6.2f | Perim.: %6.2f\n", object.getArea(), object.getPerimeter());
     }
 }
 
@@ -28,7 +35,7 @@ class Rectangle {
 
     double getHeight() { return this.height; }
     double getWidth() { return this.width; }
-    double getArea() { return this.height * this.width }
-    double getPerimeter() { return this.height + this.width * 2.0 }
+    double getArea() { return this.height * this.width; }
+    double getPerimeter() { return this.height + this.width * 2.0; }
 
 }
--- chap9/Exercise09_07.java
+++ chap9/Exercise09_07.java
@@ -1,41 +1,67 @@
-public class Exercise09_01 {
+/*
+Author: Aiden Olsen
+Date: 02/28/2025
+
+This program is for an Account class example
+ */
+
+public class Exercise09_07 {
     public static void main(String[] args) {
-        Rectangle rectA = new Rectangle(40.0, 4.0);
-        printRectangle("A", rectA);
-        Rectangle rectB = new Rectangle(35.9, 3.5);
-        printRectangle("B", rectB);
-        Rectangle[] rectArray = new Rectangle[10];
-        for (int i = 0; i < 10; i++) {
-            double rectheight = Math.random() * 11.0;
-            double rectwidth = Math.random() * 11.0;
-            rectArray[i] = new Rectangle(rectheight, rectwidth);
-            printRectangle(Integer.toString(i + 1), rectArray[i]);
-        }
+        Account account1 = new Account(1122, 20000.0);
+        account1.setAnnualInterestRate(4.5);
+        account1.withdraw(2500.0);
+        account1.deposit(3000.0);
+        printAccount(account1);
     }
-    static void printRectangle(String name, Rectangle object) {
-        System.out.printf("          Rectangle %2s\n", name);
-        System.out.printf("Height: %6.2f | Width : %6.2f\n", object.getHeight(), object.getWidth());
-        System.out.printf("Area  : %6.2f | Perim.: %6.2f\n", object.getArea(), object.getPerimeter());
+
+    public static void printAccount(Account account) {
+        System.out.printf("\nAccount #%d\n", account.getId());
+        System.out.printf("Created %s\n\n", account.getDateCreated().toString());
+        System.out.printf("\tCurrent Balance :  $%.2f\n", account.getBalance());
+        System.out.printf("\tAnnual Int. Rate:  %.2f%%\n", account.getAnnualInterestRate());
+        System.out.printf("\tMonthly Interest: +$%.2f\n\n", account.getMonthlyInterest());
     }
 }
 
-class Rectangle {
-    private double height;
-    private double width;
+class Account {
+    // Attributes
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private java.util.Date dateCreated = new java.util.Date();
 
-    Rectangle() {
-        this.height = 1.0;
-        this.width = 1.0;
+    // Constructors
+    public Account() {
+    }
+    public Account(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
     }
 
-    Rectangle(double height, double width) {
-        this.height = height;
-        this.width = width;
+    // Accessor methods for id/bal/air/date
+    public int getId() { return this.id; }
+    public double getBalance() { return this.balance; }
+    public double getAnnualInterestRate() { return this.annualInterestRate; }
+    public java.util.Date getDateCreated() { return this.dateCreated; }
+
+    // Mutator methods for id/bal/air
+    public void setId(int id) { this.id = id; }
+    public void setBalance(double balance) { this.balance = balance; }
+    public void setAnnualInterestRate(double annualInterestRate) { this.annualInterestRate = annualInterestRate; }
+
+    // Action methods for monthly interest and money manipulation
+    public double getMonthlyInterestRate() {
+        return this.annualInterestRate / 12.0;
     }
-
-    double getHeight() { return this.height; }
-    double getWidth() { return this.width; }
-    double getArea() { return this.height * this.width; }
-    double getPerimeter() { return this.height + this.width * 2.0; }
-
+    public double getMonthlyInterest() {
+        return balance * (this.annualInterestRate / 1200.0);
+    }
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.printf("Deposited $%.2f.\n", amount);
+    }
+    public void withdraw(double amount) {
+        this.balance -= amount;
+        System.out.printf("Withdrew $%.2f.\n", amount);
+    }
 }
