1059c1a39a11db91eed26fb65760af33c3a3b4e5
Avery Olsen <averyo@Averys-MacBook-Pro.local>
2025-03-07 13:00:28 -0700

    Exercise12_03, Exercise12_15

--- chap12/Exercise12_03.java
+++ chap12/Exercise12_03.java
@@ -1,1 +1,35 @@
-public class Exercise()
+/*
+Author: Aiden Olsen
+Date: 03/07/2025
+
+This program is for an exception catching example
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Exercise12_03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+        int givenValue;
+        String clearInput;
+        for (int i = 0; i < 100; i++)
+            intArrayList.add((int)(Math.random() * 101));
+        while (true) {
+            System.out.print("[!!] 100-element array made\n[!!] Input an index, or -1 to exit\n[#?] ");
+            try {
+                givenValue = input.nextInt();
+                if (givenValue == -1)
+                    break;
+                System.out.printf("[%d] - %d\n\n", givenValue, intArrayList.get(givenValue));
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.print("[XX] Invalid input. Must be -1 to 99.\n\n");
+            } catch (InputMismatchException ex) {
+                System.out.print("[XX] Invalid input. Must be an integer.\n\n");
+                clearInput = input.nextLine();
+            }
+        }
+    }
+}
--- chap12/Exercise12_15.java
+++ chap12/Exercise12_15.java
@@ -1,4 +1,41 @@
-package chap12;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Exercise12_15 {
+    public static void main(String[] args) throws Exception {
+        java.io.File file = new java.io.File("intFile");
+        if (file.exists()) {
+            System.out.print("intFile.txt already exists.\n");
+            System.exit(1);
+        } else
+            System.out.print("Creating intFile.txt ...\n");
+        try (
+            java.io.PrintWriter output = new java.io.PrintWriter(file);
+        ) {
+            output.printf("%d", 10 + (int)(Math.random() * 90)); //10-99
+            for (int i = 0; i < 99; i++)
+                output.printf(" %d", 10 + (int)(Math.random() * 90)); //10-99
+        }
+        Scanner input = new Scanner(file);
+        System.out.print("\t  Unsorted:\n");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("\t| ");
+            for (int j = 0; j < 25; j++)
+                System.out.printf("%d ", input.nextInt());
+            System.out.print("|\n");
+        }
+        input = new Scanner(file);
+        System.out.print("\t  Sorted:\n");
+        int[] tempArray = new int[100];
+        for (int i = 0; i < 100; i++)
+            tempArray[i] = input.nextInt();
+        Arrays.sort(tempArray);
+        int spot = 0;
+        for (int i = 0; i < 4; i++) {
+            System.out.print("\t| ");
+            for (int j = 0; j < 25; j++)
+                System.out.printf("%d ", tempArray[spot++]);
+            System.out.print("|\n");
+        }
+    }
 }
